458a31c876477022156752040202eb6b5dc36016
Julian Schlenker <schlenkerjulian@outlook.com>
2022-03-04 21:46:53 +0100

    feat: update header and home

--- src/others/components/Header.tsx
+++ src/others/components/Header.tsx
@@ -3,28 +3,48 @@ import { useNavigate } from "react-router-dom";
 
 import styles from "./Header.module.css";
 import { LanguageSelector } from "./LanguageSelector";
+import { Text } from "./Text";
 
 import backIcon from "../../medias/images/UGT_Asset_UI_Back.svg";
+import shareIcon from "../../medias/images/UGT_Asset_UI_Share_Icon.svg";
+import { Spacer } from "./Spacer";
 
 export interface HeaderProps {
   hasBackButton?: boolean;
+  hasAbout?: boolean;
+  hasShare?: boolean;
 }
 
-export const Header: React.FunctionComponent<HeaderProps> = ({ hasBackButton = false }) => {
+export interface HeaderProps {}
+
+export const Header: React.FunctionComponent<HeaderProps> = ({ hasBackButton = false, hasAbout = false, hasShare = false }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   return (
     <nav className={styles.wrapper}>
+      {hasAbout && (
+        <div className={styles.headerCard}>
+          <Text>About us</Text>
+        </div>
+      )}
       <div>
         {hasBackButton && (
-          <span className={styles.backButton} onClick={() => navigate(-1)}>
+          <div className={styles.headerItem} onClick={() => navigate(-1)}>
             <img src={backIcon} alt={t("back")} className={styles.backIcon} />
             {t("back")}
-          </span>
+          </div>
         )}
       </div>
-      <LanguageSelector />
+      <Spacer flex={1} />
+      <div className={styles.headerCard}>
+        <LanguageSelector />
+      </div>
+      {hasShare && (
+        <div className={styles.headerCard}>
+          <img className={styles.shareIcon} src={shareIcon} alt={t("share")} />
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,10 +4,15 @@ import { useTranslation } from "react-i18next";
 
 import { Button } from "../../others/components/Button";
 import { Card } from "../../others/components/Card";
-import { LanguageSelector } from "../../others/components/LanguageSelector";
+
 import { List } from "../../others/components/List";
 import { Spacer } from "../../others/components/Spacer";
 import { Text } from "../../others/components/Text";
+
+import styles from "./home.module.css";
+import ugtLogo from "../../medias/images/UGT_Asset_Brand.svg";
+import nextIcon from "../../medias/images/UGT_Asset_UI_ButtonIndication.svg";
+import { Header } from "../../others/components/Header";
 
 export function Home() {
   const { t } = useTranslation();
@@ -15,17 +20,27 @@ export function Home() {
 
   return (
     <React.Fragment>
-      <LanguageSelector />
+      <Header hasAbout hasShare />
+      <Spacer size={40} />
+      <img className={styles.ugtLogo} src={ugtLogo} alt="UGT Logo" />
       <h1>{t("home_how_does_works")}</h1>
       <List>
-        <li><Text>{t("home_we_help_authorities")}</Text></li>
-        <li><Text>{t("home_tell_us")}</Text></li>
-        <li><Text>{t("home_deliver_supplies")}</Text></li>
+        <li>
+          <Text variant="bold">{t("home_tell_us")}</Text>
+        </li>
+        <li>
+          <Text variant="bold">{t("home_we_help_authorities")}</Text>
+        </li>
+        <li>
+          <Text variant="bold">{t("home_deliver_supplies")}</Text>
+        </li>
       </List>
       <Card>
         <Text alignment="center">{t("home_acknowledgement")}</Text>
-        <Spacer />
-        <Button fullWidth onClick={() => navigate("/locator")}>{t("home_i_understand")}</Button>
+        <Spacer size={22} />
+        <Button fullWidth onClick={() => navigate("/locator")} trailingIcon={<img src={nextIcon} alt="" />}>
+          {t("home_i_understand")}
+        </Button>
       </Card>
     </React.Fragment>
   );
