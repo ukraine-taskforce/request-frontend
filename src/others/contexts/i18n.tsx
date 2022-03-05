7d7fdb90aee73701d7cea783228a74de6ef617a4
Julian Schlenker <schlenkerjulian@outlook.com>
2022-03-05 22:25:19 +0100

    feat: init lang from navigator

--- src/others/contexts/i18n.tsx
+++ src/others/contexts/i18n.tsx
@@ -9,11 +9,17 @@ const resources = {
   uk: { translation: { ...uk } },
 };
 
+function getInitLang() {
+  const availableLangs = Object.keys(resources);
+  const browserLang = navigator.language.split("-")[0].toLowerCase();
+  return availableLangs.includes(browserLang) ? browserLang : "en";
+}
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
+    lng: getInitLang(),
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
