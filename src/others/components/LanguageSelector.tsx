b24d5973e4f6de37b03cd281c9a31c9674e17297
Theo Dammaretz <theo@dammaretz.fr>
2022-03-05 15:22:03 +0100

    fix: add flags for language selector

--- src/others/components/LanguageSelector.tsx
+++ src/others/components/LanguageSelector.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import UKRFlag from "../../medias/images/UGT_Asset_FlagSelector_UKR.svg";
-import ENFlag from "../../medias/images/UGT_Asset_FlagSelector_UKR.svg";
+import ENFlag from "../../medias/images/UGT_Asset_FlagSelector_ENG.svg";
 import styles from "./LanguageSelector.module.css";
 
 export interface LanguageSelectorProps {}
@@ -12,10 +12,13 @@ const flagMap = {
 
 export const LanguageSelector: React.FunctionComponent<LanguageSelectorProps> = () => {
   const { i18n } = useTranslation();
-  const lang = i18n.language === "en" ? "uk" : "en";
+
+  const nextLang = i18n.language === "en" ? "uk" : "en";
+  const currentLang = i18n.language === "en" ? "en" : "uk";
+
   return (
-    <div onClick={() => i18n.changeLanguage(lang)}>
-      <img className={styles.flagIcon} src={flagMap[lang]} alt="" />
+    <div onClick={() => i18n.changeLanguage(nextLang)}>
+      <img className={styles.flagIcon} src={flagMap[currentLang]} alt="" />
       {i18n.language.toUpperCase()}
     </div>
   );
