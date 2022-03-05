d3407e4c65a0c5655ed0a31db44ecf2afa752835
Julian Schlenker <schlenkerjulian@outlook.com>
2022-03-05 23:28:55 +0100

    feat: Button label always center

--- src/others/components/Button.tsx
+++ src/others/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Button.module.css";
+import { Spacer } from "./Spacer";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "normal" | "highlight" | "white";
@@ -25,9 +26,12 @@ export const Button: React.FunctionComponent<ButtonProps> = ({
       style={{ width: fullWidth ? "100%" : "auto" }}
       {...props}
     >
-      {Boolean(leadingIcon) && <span className={styles.leadingIcon}>{leadingIcon}</span>}
       <span className={styles.label}>{children}</span>
-      {Boolean(trailingIcon) && <span className={styles.trailingIcon}>{trailingIcon}</span>}
+      <div className={styles.iconsContainer}>
+        {Boolean(leadingIcon) && <span className={styles.leadingIcon}>{leadingIcon}</span>}
+        <Spacer flex={1}></Spacer>
+        {Boolean(trailingIcon) && <span className={styles.trailingIcon}>{trailingIcon}</span>}
+      </div>
     </button>
   );
 };
