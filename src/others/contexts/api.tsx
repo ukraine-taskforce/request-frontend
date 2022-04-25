57ea9612c57ddc4093f382b7fab5a6b08743515a
Theo Dammaretz <theo@dammaretz.fr>
2022-04-25 09:06:10 +0200

    feat: init auth

--- src/others/contexts/api.tsx
+++ src/others/contexts/api.tsx
@@ -1,8 +1,10 @@
-import { useTranslation } from "react-i18next";
-import { QueryClient, useMutation, useQuery } from "react-query";
+import React from "react"
+import {useTranslation} from "react-i18next";
+import {QueryClient, useMutation, useQuery} from "react-query";
 
-import { FormData } from "./form";
-import { generateCaptchaToken } from "./recaptcha";
+import {FormData} from "./form";
+import {generateCaptchaToken} from "./recaptcha";
+import {useAuth} from "./auth";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,17 +18,44 @@ export const API_DOMAIN = process.env.REACT_APP_API_DOMAIN || "http://127.0.0.1"
 
 export type ID = string | number;
 
+function useFetch() {
+  const {forceSessionRefresh, session} = useAuth();
+
+  const query = React.useCallback(
+    (input: RequestInfo, init?: RequestInit) => {
+      return fetch(input, {
+        headers: {
+          Authorization: session?.accessToken.jwtToken || "",
+        },
+        ...init,
+      }).then((res) => {
+        if (res.status === 401) {
+          forceSessionRefresh();
+        }
+
+        return res;
+      });
+    },
+    [forceSessionRefresh, session]
+  );
+
+  return {
+    query,
+  };
+}
+
 export interface Location {
   id: ID;
   name: string;
 }
 
 export function useLocationsQuery() {
-  const { i18n } = useTranslation();
+  const {i18n} = useTranslation();
+  const {query} = useFetch();
 
   return useQuery<Location[]>(`locationQuery${i18n.language}`, async () => {
     try {
-      const result = await fetch(`${API_DOMAIN}/locations?locale=${i18n.language}`)
+      const result = await query(`${API_DOMAIN}/locations?locale=${i18n.language}`)
         .then((res) => {
           if (!res.ok) throw new Error(res.statusText);
 
@@ -38,10 +67,10 @@ export function useLocationsQuery() {
     } catch (error) {
       if (process.env.NODE_ENV !== "production") {
         return [
-          { id: 1, name: "Kyiv" },
-          { id: 2, name: "Kyinka" },
-          { id: 3, name: "Kyrnasivka" },
-          { id: 4, name: "Kyrylivka" },
+          {id: 1, name: "Kyiv"},
+          {id: 2, name: "Kyinka"},
+          {id: 3, name: "Kyrnasivka"},
+          {id: 4, name: "Kyrylivka"},
         ];
       }
 
@@ -56,11 +85,12 @@ export interface Supply {
 }
 
 export function useSuppliesQuery() {
-  const { i18n } = useTranslation();
+  const {i18n} = useTranslation();
+  const {query} = useFetch();
 
   return useQuery<Supply[]>(`suppliesQuery${i18n.language}`, async () => {
     try {
-      const result = await fetch(`${API_DOMAIN}/supplies?locale=${i18n.language}`)
+      const result = await query(`${API_DOMAIN}/supplies?locale=${i18n.language}`)
         .then((res) => {
           if (!res.ok) throw new Error(res.statusText);
 
@@ -68,14 +98,14 @@ export function useSuppliesQuery() {
         })
         .then((res) => res.json());
 
-      return result.supplies.filter((supply: Supply) => Boolean(supply.name)  && Boolean(supply.id));
+      return result.supplies.filter((supply: Supply) => Boolean(supply.name) && Boolean(supply.id));
     } catch (error) {
       if (process.env.NODE_ENV !== "production") {
         return [
-          { id: 1, name: "Food" },
-          { id: 2, name: "Water" },
-          { id: 3, name: "Baby Food" },
-          { id: 4, name: "Medical Kits / Supplies" },
+          {id: 1, name: "Food"},
+          {id: 2, name: "Water"},
+          {id: 3, name: "Baby Food"},
+          {id: 4, name: "Medical Kits / Supplies"},
         ];
       }
 
@@ -85,12 +115,13 @@ export function useSuppliesQuery() {
 }
 
 export function useSubmitMutation() {
-  const { i18n } = useTranslation();
+  const {i18n} = useTranslation();
+  const {query} = useFetch();
 
   return useMutation("submitMutation", async (formData: FormData) => {
     try {
       const recaptchaToken = await generateCaptchaToken("submit");
-      const result = await fetch(`${API_DOMAIN}?locale=${i18n.language}`, {
+      return await query(`${API_DOMAIN}?locale=${i18n.language}`, {
         method: "POST",
         body: JSON.stringify({
           ...formData,
@@ -103,8 +134,6 @@ export function useSubmitMutation() {
           return res;
         })
         .then((res) => res.json());
-
-      return result;
     } catch (error) {
       if (process.env.NODE_ENV !== "production") {
         return null;
---
+++ src/others/contexts/auth.tsx
@@ -0,0 +1,202 @@
+import { AuthenticationDetails, CognitoUser, CognitoUserPool, CognitoUserSession } from "amazon-cognito-identity-js";
+import React from "react";
+
+const userPoolId = process.env.REACT_APP_USERPOOL_ID || "";
+const clientId = process.env.REACT_APP_CLIENT_ID || "";
+
+const userPool = new CognitoUserPool({ UserPoolId: userPoolId, ClientId: clientId });
+
+export enum AuthStatus {
+  SignedIn = "SignedIn",
+  SignedOut = "SignedOut",
+  Loading = "Loading",
+}
+
+interface Session extends CognitoUserSession {
+  accessToken: { jwtToken: string };
+}
+
+export interface AuthContextValue {
+  status: AuthStatus;
+  user: CognitoUser | null;
+  forceSessionRefresh: () => Promise<void>;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  sendCode: (username: string) => Promise<void>;
+  session: Session | null;
+  confirmPassword: (code: string, username: string, newPassword: string) => Promise<void>;
+}
+
+const AuthContext = React.createContext<AuthContextValue>({
+  status: AuthStatus.Loading,
+  user: null,
+  session: null,
+  login: () => Promise.resolve(),
+  logout: () => Promise.resolve(),
+  sendCode: () => Promise.resolve(),
+  confirmPassword: () => Promise.resolve(),
+  forceSessionRefresh: () => Promise.resolve(),
+});
+
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+async function getUserSession(): Promise<Session> {
+  const currentUser = userPool.getCurrentUser();
+
+  return new Promise(function (resolve, reject) {
+    if (!currentUser) {
+      reject();
+    }
+
+    currentUser?.getSession((error: any, session: any) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(session);
+      }
+    });
+  });
+}
+
+async function sendUserResetCode(username: string) {
+  return new Promise(function (resolve, reject) {
+    const nextUser = new CognitoUser({
+      Username: username,
+      Pool: userPool,
+    });
+
+    nextUser.forgotPassword({
+      onSuccess: () => resolve(null),
+      onFailure: (err: any) => reject(err),
+    });
+  });
+}
+
+async function changeUserPassword(code: string, username: string, newPassword: string) {
+  return new Promise(function (resolve, reject) {
+    const nextUser = new CognitoUser({
+      Username: username,
+      Pool: userPool,
+    });
+
+    nextUser.confirmPassword(code, newPassword, {
+      onSuccess: () => resolve(null),
+      onFailure: (err: any) => reject(err),
+    });
+  });
+}
+
+export const AuthProvider: React.FunctionComponent = ({ children }) => {
+  const [status, setStatus] = React.useState<AuthStatus>(AuthStatus.Loading);
+  const [session, setSession] = React.useState<Session | null>(null);
+
+  React.useEffect(() => {
+    const getCurrentSession = async () => {
+      try {
+        const nextSession = await getUserSession();
+
+        // In case we are in the initial loading of the page, we try to refresh the session
+        if (status === AuthStatus.Loading && nextSession) {
+          userPool.getCurrentUser()?.refreshSession(nextSession.getRefreshToken(), (err) => {
+            if (err) console.error("Error refreshing session", err);
+          });
+        }
+
+        setSession(nextSession);
+        setStatus(AuthStatus.SignedIn);
+      } catch (err) {
+        setSession(null);
+        setStatus(AuthStatus.SignedOut);
+      }
+    };
+    getCurrentSession();
+  }, [status, setStatus]);
+
+  const login = React.useCallback(
+    async (username: string, password: string) => {
+      if (status !== AuthStatus.SignedOut) {
+        throw new Error("A user is already signed in");
+      }
+
+      const authenticationDetails = new AuthenticationDetails({
+        Username: username,
+        Password: password,
+      });
+      const nextUser = new CognitoUser({
+        Username: username,
+        Pool: userPool,
+      });
+
+      try {
+        await new Promise((resolve, reject) => {
+          nextUser.authenticateUser(authenticationDetails, {
+            onSuccess: () => {
+              setStatus(AuthStatus.SignedIn);
+              resolve(null);
+            },
+            onFailure: (err: any) => reject(err),
+            newPasswordRequired: () => {
+              nextUser.completeNewPasswordChallenge(
+                password,
+                {},
+                {
+                  onSuccess: () => {},
+                  onFailure: () => {},
+                }
+              );
+              resolve(null);
+            },
+          });
+        });
+      } catch (error) {
+        setStatus(AuthStatus.SignedOut);
+        throw error;
+      }
+    },
+    [status, setStatus]
+  );
+
+  const logout = React.useCallback(async () => {
+    userPool.getCurrentUser()?.signOut();
+    setStatus(AuthStatus.SignedOut);
+  }, [setStatus]);
+
+  const sendCode = React.useCallback(async (username: string) => {
+    try {
+      await sendUserResetCode(username);
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
+  const confirmPassword = React.useCallback(async (code: string, username: string, newPassword: string) => {
+    try {
+      await changeUserPassword(code, username, newPassword);
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
+  const forceSessionRefresh = React.useCallback(async () => {
+    setStatus(AuthStatus.Loading);
+  }, [setStatus]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        forceSessionRefresh,
+        login,
+        logout,
+        sendCode,
+        confirmPassword,
+        status,
+        session,
+        user: userPool.getCurrentUser(),
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
