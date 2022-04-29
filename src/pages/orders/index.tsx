a5a995f79c6b9b2eb0edf9f9c643fdaf3a088ee2
Alison Goryachev <alisonmllr20@gmail.com>
2022-04-28 20:55:07 -0400

    cleanup orders page

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 import ReactGA from "react-ga4";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionProps,
+  AccordionSummaryProps,
+  AccordionDetails,
+  Typography,
+  Box,
+  Stack,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import { styled } from "@mui/material/styles";
 import { useTranslation } from "react-i18next";
 import { Header } from "../../others/components/Header";
 import { Card } from "../../others/components/Card";
@@ -13,22 +26,58 @@ import { RequestStatus, fakeRequests } from "../../others/helpers/requests";
 import { useLocationsQuery, useSuppliesQuery } from "../../others/contexts/api";
 import styles from "./orders.module.css";
 
+const CustomAccordion = styled((props: AccordionProps) => <Accordion disableGutters elevation={0} square {...props} />)(({ theme }) => ({
+  borderRadius: "8px",
+  marginBottom: "20px",
+  boxShadow: "0px 1px 8px 2px #14141414",
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const CustomAccordionSummary = styled((props: AccordionSummaryProps) => <AccordionSummary expandIcon={<ExpandMoreIcon />} {...props} />)(
+  ({ theme }) => ({
+    flexDirection: "row-reverse",
+    padding: "10px 16px",
+    "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+      transform: "rotate(180deg)",
+    },
+    "& .MuiAccordionSummary-content": {
+      marginLeft: theme.spacing(1),
+    },
+  })
+);
+
+const CustomAccordionDetails = styled(AccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
 export function Orders() {
   const { t } = useTranslation();
   const { logout } = useAuth();
-  const [visibleRequestId, setVisibleRequestId] = useState(0);
+  const [expandedRequest, setExpandedRequest] = useState<string | false>(false);
   const { data: cities } = useLocationsQuery();
   const { data: supplies } = useSuppliesQuery();
 
   // TODO: If we don't replace it with a better mechanism at least use useMemo.
-  const cityLookup = cities ? Object.assign({}, ...cities.map((city) => ({[city.id]: city }))) : {};
-  const supplyLookup = supplies ? Object.assign({}, ...supplies.map((supply) => ({[supply.id]: supply }))): {};
+  const cityLookup = cities ? Object.assign({}, ...cities.map((city) => ({ [city.id]: city }))) : {};
+  const supplyLookup = supplies ? Object.assign({}, ...supplies.map((supply) => ({ [supply.id]: supply }))) : {};
+
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+    setExpandedRequest(newExpanded ? panel : false);
+  };
 
   useEffect(() => {
     document.title = t("orders_page_title");
     ReactGA.send("pageview");
   }, [t]);
- 
+
+  // TODO need to properly handle error and loading states
+  if (!cities || !supplies) {
+    return null;
+  }
+
   return (
     <React.Fragment>
       <Header hasHeadline hasLangSelector />
@@ -37,35 +86,50 @@ export function Orders() {
         <Tooltip title="Logout" arrow>
           <OutputIcon onClick={logout} sx={{ width: 30, height: 30, marginLeft: "auto", cursor: "pointer" }} />
         </Tooltip>
-          <Spacer size={24} />
-          <div className={styles.flex}>
-            {fakeRequests.map((request) => (
-              <React.Fragment key={request.id}>
-                <Card className={styles.card} onClick={() => setVisibleRequestId(request.id)}>
-                  <Text variant="bold">Request #{request.id}</Text>
-                </Card>
-                { visibleRequestId === request.id && <Card>
-                  {cityLookup[request.city_id].name}<br/>
-                  {request.userName}<br/>
-                  {request.userPhoneNumber}<br/>
-                  <br/>
-                  {request.supplies.map((supply) => (
-                    <Text key={supply.id}>
-                      {supplyLookup[supply.id].name}: {supply.amount}
-                    </Text>
-                  ))}
 
-                  <br/><br/>
-                  Date: <br/>
-                  {request.date.toLocaleString()}<br/><br/>
-                  Status: <br/>
-                  {RequestStatus[request.status]}
-                </Card> }
-                <Spacer size={6} />
-              </React.Fragment>
-            ))}
-          </div>
-        </Content>
+        <Spacer size={24} />
+
+        <div>
+          {fakeRequests.map((request) => (
+            <CustomAccordion
+              expanded={expandedRequest === `panel-${request.id}`}
+              onChange={handleChange(`panel-${request.id}`)}
+              key={request.id}
+            >
+              <CustomAccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                <Text variant="bold">Request #{request.id}</Text>
+              </CustomAccordionSummary>
+              <CustomAccordionDetails>
+                <Stack spacing={2}>
+                  <Text className={styles.contactListItem}>{cityLookup[request.city_id].name}</Text>
+                  <Text className={styles.contactListItem}>{request.userName}</Text>
+                  <Text className={styles.contactListItem}>{request.userPhoneNumber}</Text>
+                </Stack>
+
+                <Stack spacing={2}>
+                  <dl>
+                    {request.supplies.map((supply) => (
+                      <React.Fragment key={supply.id}>
+                        <dt>{supplyLookup[supply.id].name}</dt>
+                        <dd>{supply.amount}</dd>
+                      </React.Fragment>
+                    ))}
+                  </dl>
+                </Stack>
+
+                <Stack spacing={2}>
+                  <dl>
+                    <dt>Date</dt>
+                    <dd>{request.date.toLocaleString()}</dd>
+                    <dt>Status</dt>
+                    <dd>{RequestStatus[request.status]}</dd>
+                  </dl>
+                </Stack>
+              </CustomAccordionDetails>
+            </CustomAccordion>
+          ))}
+        </div>
+      </Content>
     </React.Fragment>
   );
 }
